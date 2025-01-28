b5de20996b0de7cf35d72cc7750e2278c4b413b9
jasdvl <166537903+jasdvl@users.noreply.github.com>
2025-01-28 23:16:39 +0100

    refactor(config): update default server host and model path handling
    - Change default server host to 0.0.0.0 to allow external access
    - Simplify model path handling with a predefined relative path

--- src/AIxplorer.GenAI.ImageGen/config.py
+++ src/AIxplorer.GenAI.ImageGen/config.py
@@ -14,18 +14,17 @@ load_dotenv(override=True)
 
 # Default values
 
-DEFAULT_SERVER_HOST = "127.0.0.1"
+DEFAULT_SERVER_HOST = "0.0.0.0"
 DEFAULT_SERVER_PORT = 7600
-
 # Path to Stable Diffusion model
-relative_model_path = os.getenv("MODEL_PATH")
+RELATIVE_MODEL_PATH = "../../../../AI-Models/stable-diffusion-3.5-medium"
 
-if not relative_model_path:
-    raise ValueError("MODEL_PATH is not set in the environment variables. Please set it in your .env file.")
+# Convert relative model path to absolute path
+ABS_MODEL_PATH = os.path.abspath(RELATIVE_MODEL_PATH)
+
 
 # Get environment variables, with default values if they are not set
+MODEL_PATH = os.getenv("MODEL_PATH", ABS_MODEL_PATH)
 SERVER_HOST = os.getenv("SERVER_HOST", DEFAULT_SERVER_HOST)
 SERVER_PORT = int(os.getenv("SERVER_PORT", DEFAULT_SERVER_PORT))
 
-# Convert relative model path to absolute path
-MODEL_PATH = os.path.abspath(relative_model_path)
